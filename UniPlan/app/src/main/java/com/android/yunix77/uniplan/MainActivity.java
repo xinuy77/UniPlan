98cef56fbd8f9ff9b7f715fa0a5c5451de8702dc
akiencore <akiencore@qq.com>
2017-10-22 09:20:12 +0800

    Update MainActivity.java

--- UniPlan/app/src/main/java/com/android/yunix77/uniplan/MainActivity.java
+++ UniPlan/app/src/main/java/com/android/yunix77/uniplan/MainActivity.java
@@ -1,8 +1,13 @@
 package com.android.yunix77.uniplan;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,21 +17,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener ,
+        appTitleInterface{
 
     private DatabaseControl dbControl;
     private DatabaseTester test;
+                
+    protected Fragment eventFragment;
+    protected Fragment homeFragment;
+    protected Fragment placesFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        eventFragment   = new eventFragment();
+        homeFragment    = new homeFragment();
+        placesFragment  = new placesFragment();
+            
         dbControl = new DatabaseControl(getApplicationContext());
         test      = new DatabaseTester(dbControl);
         initUI();
+    }
+                
+    public void ChangeFragment(int id){
+        Fragment currentFragment = null;
+        if(id==R.id.nav_home){
+            currentFragment = homeFragment;
+        }
+        else if(id==R.id.nav_events){
+            currentFragment = eventFragment;
+        }
+        else if(id==R.id.nav_places){
+            currentFragment = placesFragment;
+        }
+        if(currentFragment != null){
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.include, currentFragment);
+            ft.addToBackStack(null); //default is the previous one
+            ft.commit();
+        }
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
     }
 
     public void initUI() {
@@ -41,6 +78,13 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+            
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.include, homeFragment);
+        ft.commit();
+
+        ChangeFragment(R.id.nav_home);
     }
 
     @Override
@@ -81,8 +125,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        ChangeFragment(id);
+            
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    
+    @Override
+    public void onSetTitle(String title) {
+        setTitle(title);
     }
 }
