aa647db3e310d45ece8749ac5a4e73f69f8f4298
xion77 <yuyamanakajpn0120@gmail.com>
2017-10-25 18:49:10 -0400

    cleaned

---
+++ debug/UniPlan/app/build/generated/source/buildConfig/debug/com/android/yunix77/uniplan/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.android.yunix77.uniplan;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.android.yunix77.uniplan";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
